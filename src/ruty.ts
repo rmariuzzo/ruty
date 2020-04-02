d2f69a59fa5dddf684dbd1988c67f697a3f339b9
Rubens Mariuzzo <rubens@mariuzzo.com>
2020-04-02 15:15:08 -0400

    Add prettier.

--- src/ruty.ts
+++ src/ruty.ts
@@ -1,20 +1,20 @@
 const addParams = (path: string, params: { [key: string]: string } = {}) => {
-  return path.replace(/:(\w+)/g, (token, key) => params[key] || token);
-};
+  return path.replace(/:(\w+)/g, (token, key) => params[key] || token)
+}
 
 const addQueries = (path: string, queries: { [key: string]: string } = {}) => {
   return path.replace(/[?&](\w+)/g, (token, key) =>
     queries[key] === undefined ? token : `${token}=${queries[key]}`
-  );
-};
+  )
+}
 
 export const route = <Path extends string = string>(path: Path) => {
   const build = <ParamsAndQueries extends { [key: string]: any } = {}>() => {
     const routeBuilder = (paramsAndQueries?: ParamsAndQueries) => {
-      return addQueries(addParams(path, paramsAndQueries), paramsAndQueries);
-    };
-    routeBuilder.path = path;
-    return routeBuilder;
-  };
-  return { build };
-};
+      return addQueries(addParams(path, paramsAndQueries), paramsAndQueries)
+    }
+    routeBuilder.path = path
+    return routeBuilder
+  }
+  return { build }
+}
